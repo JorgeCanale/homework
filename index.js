cdd8857757444be2b1d2c0aee6f34aa2d19c5ab2
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-08-27 12:44:55 -0500

    Mike's function

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
   if (numero % 2 === 0) {
     return numero.toString(2);
   } else {
     return numero.toString(16);
   }
+}
+
+function mike() {
+  const mike = {
+    name: "Mike",
+    age: 25,
+    nationality: "Colombian"
+  }
+  return mike
 }

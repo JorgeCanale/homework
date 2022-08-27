bd258c7f15504a7ffaaa13c1458f2a4fc93941d3
JorgeCanale <jorgecanale636@yahoo.com>
2022-08-27 14:48:53 -0300

    cree las funciones pero no su funcionalidad

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function laCajaDePandora(numero){
+  // proximamente escribiremos codigo aqui
+  let espar = numero % 2 === 0 ? true : false
 
+  if(espar){
+    return espar
+  }else{
+    return espar
+  }
+
+  funciton benja(){
+    return{
+        nombre: 'jorge rodolgo benjamin canale',
+        edad: 22,
+        nacionalidad: 'argentino'
+    }
+  }
 }
